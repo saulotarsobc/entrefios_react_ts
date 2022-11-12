593e3734bb3e0e1da8b6f041facb044c2a721f69
saulotarsobc <saulotarsobc@gmail.com>
2022-11-11 22:26:19 -0300

    :fire:

---
+++ src/api/Bolsas/index.ts
@@ -0,0 +1,11 @@
+import bolsa_1 from "./images/1.jpg";
+import bolsa_2 from "./images/2.jpg";
+import bolsa_3 from "./images/3.jpg";
+
+const Bolsas = [
+  { src: bolsa_1, src2: bolsa_1, alt: "Bolsa Margarida" },
+  { src: bolsa_2, src2: bolsa_2, alt: "Bolsa Cacto" },
+  { src: bolsa_3, src2: bolsa_3, alt: "Bolsa Petûnia" },
+];
+
+export default Bolsas;
--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -4,14 +4,28 @@ import Images from "../../api/Images";
 import Thumb from "../../components/Thumb";
 import Modal from "../../components/Modal";
 import SubTitle from "../../components/SubTitle";
+import Bolsas from "../../api/Bolsas";
 
 function Catalog() {
   return (
     <section id="catalog">
       <Modal />
-      <SubTitle subtitle="Catálogo" />
+
+      <SubTitle subtitle="Cestinhos" />
       <div className="catalog_wrap">
         {Images.map((image, index) => (
+          <Thumb
+            key={index}
+            src={image.src}
+            src2={image.src2}
+            alt={image.alt}
+          />
+        ))}
+      </div>
+
+      <SubTitle subtitle="Bolsas" />
+      <div className="catalog_wrap">
+        {Bolsas.map((image, index) => (
           <Thumb
             key={index}
             src={image.src}
